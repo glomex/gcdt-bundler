b8f53af19e215befbf7f34880b038c9a17c1d553
Jenkins Continuous Integration Server <sre-chapter@de.glomex.com>
2017-06-22 21:18:00 +0000

    Bump version: 0.0.489 → 0.0.490

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' no
 
 setup(
     name='gcdt-bundler',
-    version='0.0.489',
+    version='0.0.490',
     description='Plugin (gcdt-bundler) for gcdt',
     long_description=long_description,
     license='MIT',
